cf765cf99282f29e9a25cb4e052355cf43b6c7c9
Vedant <vedantbhatt335@gmail.com>
2021-08-23 21:15:52 +0530

    Updates

---
+++ precache-manifest.6870f88bc5576ace2df6dc323f293361.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb49b66ac9b8c1d77e64818a3452b48",
+    "url": "/react-milanote/index.html"
+  },
+  {
+    "revision": "9be06c531852ecd02e39",
+    "url": "/react-milanote/static/css/chunk-blinkmind.2ad25acf.chunk.css"
+  },
+  {
+    "revision": "573e7b0579b47d7fa0e5",
+    "url": "/react-milanote/static/css/chunk-libs.ee8e6b6e.chunk.css"
+  },
+  {
+    "revision": "9be06c531852ecd02e39",
+    "url": "/react-milanote/static/js/chunk-blinkmind.78d035a0.chunk.js"
+  },
+  {
+    "revision": "573e7b0579b47d7fa0e5",
+    "url": "/react-milanote/static/js/chunk-libs.1f2b0070.chunk.js"
+  },
+  {
+    "revision": "8d67e71ad2575aefbbb7",
+    "url": "/react-milanote/static/js/chunk-slate.b4ce9264.chunk.js"
+  },
+  {
+    "revision": "70e271ed214eaefd85f1",
+    "url": "/react-milanote/static/js/main.d31d95c9.chunk.js"
+  },
+  {
+    "revision": "d053c1c682f39dedf8e0",
+    "url": "/react-milanote/static/js/runtime~main.55424f25.js"
+  },
+  {
+    "revision": "4f8c49830f9d89080c0e14ef27fe5119",
+    "url": "/react-milanote/static/media/default.4f8c4983.jpg"
+  },
+  {
+    "revision": "1615fe237931cc9888e8da84ee2bddf9",
+    "url": "/react-milanote/static/media/iconfont.1615fe23.ttf"
+  },
+  {
+    "revision": "39b55534800824e0e0fcbc8613765f72",
+    "url": "/react-milanote/static/media/iconfont.39b55534.svg"
+  },
+  {
+    "revision": "3ea7cc77255df2a8a4203699c5c7ba8c",
+    "url": "/react-milanote/static/media/iconfont.3ea7cc77.ttf"
+  },
+  {
+    "revision": "46a2ccc0f9b5e741b0a7236a45595cfc",
+    "url": "/react-milanote/static/media/iconfont.46a2ccc0.svg"
+  },
+  {
+    "revision": "48fe2018c6493d7b6dcad333f63778e5",
+    "url": "/react-milanote/static/media/iconfont.48fe2018.woff"
+  },
+  {
+    "revision": "52b6ee751b76bedccfefc9862ac73b8c",
+    "url": "/react-milanote/static/media/iconfont.52b6ee75.woff"
+  },
+  {
+    "revision": "aa15821e33931d66ea8a44e325c97faf",
+    "url": "/react-milanote/static/media/iconfont.aa15821e.eot"
+  },
+  {
+    "revision": "c7f498034eb3b56e4545ebd2891aacc0",
+    "url": "/react-milanote/static/media/iconfont.c7f49803.eot"
+  },
+  {
+    "revision": "2f59f00b1baa3f74747694eff9e53875",
+    "url": "/react-milanote/static/media/theme1.2f59f00b.jpg"
+  },
+  {
+    "revision": "b18193fbba175e35f6fbf3391f385723",
+    "url": "/react-milanote/static/media/theme2.b18193fb.jpg"
+  },
+  {
+    "revision": "61c97910ac792963391befcf87c7ce00",
+    "url": "/react-milanote/static/media/theme3.61c97910.jpg"
+  },
+  {
+    "revision": "1e21408c23fc4c6933552278be804add",
+    "url": "/react-milanote/static/media/theme4.1e21408c.jpg"
+  }
+]);
---
+++ static/js/main.d31d95c9.chunk.js
@@ -0,0 +1,2 @@
+(window["webpackJsonpreact-mindmap"]=window["webpackJsonpreact-mindmap"]||[]).push([[3],{338:function(n,p,a){n.exports=a(857)}},[[338,4,0,2,1]]]);
+//# sourceMappingURL=main.d31d95c9.chunk.js.map
